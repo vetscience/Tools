c0ac064e581a614f2ef39eb25e0335ec0e961a88
Pasi Korhonen <pasi.korhonen@unimelb.edu.au>
2018-04-20 10:55:45 +1000

    Modifications for udocker

--- Cwl/repeatmodeler/initReps.sh
+++ Cwl/repeatmodeler/initReps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /root/Libraries
+#cd /usr/local/RepeatMasker/Libraries
 mkdir /var/spool/cwl/Libraries
 #cp RepeatPeps.readme /var/spool/cwl/Libraries
 #cp RepeatPeps.lib /var/spool/cwl/Libraries
@@ -9,17 +9,8 @@ mkdir /var/spool/cwl/Libraries
 #cp RMRBMeta.embl /var/spool/cwl/Libraries
 #cp README.meta /var/spool/cwl/Libraries
 #cp DfamConsensus.embl /var/spool/cwl/Libraries
-cp RepeatPeps.readme /var/spool/cwl
-cp RepeatPeps.lib /var/spool/cwl
-cp RepeatAnnotationData.pm /var/spool/cwl
-cp Dfam.hmm /var/spool/cwl
-cp taxonomy.dat /var/spool/cwl
-cp RMRBMeta.embl /var/spool/cwl
-cp README.meta /var/spool/cwl
-cp DfamConsensus.embl /var/spool/cwl
 # This copy circumvents the issue encountered with CWL InitialWorkDir and udocker
-#find /var/lib/cwl -name RMRBSeqs.embl -exec cp {} /var/spool/cwl/Libraries \;
-find /var/lib/cwl -name RMRBSeqs.embl -exec cp {} /var/spool/cwl \;
+find /var/lib/cwl -name RMRBSeqs.embl -exec cp {} /usr/local/RepeatMasker/Libraries \;
 #cp /var/spool/cwl/RepBaseLibrary/RMRBSeqs.embl /var/spool/cwl/Libraries
 cd /usr/local/RepeatMasker
 perl configure < /root/inputRepeatMasker
--- Cwl/repeatmodeler/repeatModeler.sh
+++ Cwl/repeatmodeler/repeatModeler.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sh /root/initReps.sh
 RepeatModeler $@
+cp /root/Libraries/* /var/spool/cwl/Libraries
